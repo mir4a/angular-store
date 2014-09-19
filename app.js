1a14e22bb0095e2056c9cf89106a2b8751e4c20b
Miroslav Martynov <marty.mircha@gmail.com>
2014-09-19 12:24:30 +0300

    Added controller

--- app.js
+++ app.js
@@ -2,4 +2,16 @@
  * Created by mir4a on 9/19/14.
  */
 
-var app = angular.module('store', []);
+// Closure is a good habbit
+(function(){
+  var app = angular.module('store', []);
+  app.controller('StoreController', function() {
+    this.product = gem;
+  });
+
+  var gem = {
+    name: 'Wow gem',
+    price: 2.65,
+    description: "Some description"
+  }
+})();
