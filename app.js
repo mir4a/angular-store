01e578c5d49142461b482236a7a9de01301f27d6
Miroslav Martynov <marty.mircha@gmail.com>
2014-09-19 13:41:59 +0300

    For loops

--- app.js
+++ app.js
@@ -2,16 +2,27 @@
  * Created by mir4a on 9/19/14.
  */
 
-// Closure is a good habbit
-(function(){
+  // Closure is a good habbit
+(function () {
   var app = angular.module('store', []);
-  app.controller('StoreController', function() {
-    this.product = gem;
+  app.controller('StoreController', function () {
+    this.products = gem;
   });
 
-  var gem = {
-    name: 'Wow gem',
-    price: 2.65,
-    description: "Some description"
-  }
+  var gem = [
+    {
+      name       : 'Wow gem',
+      price      : 2.65,
+      description: "Some description",
+      canPurchase: true,
+      soldOut    : false
+    },
+    {
+      name       : 'Gadamn gem',
+      price      : 2.34,
+      description: "Some description",
+      canPurchase: false,
+      soldOut    : false
+    }
+  ]
 })();
