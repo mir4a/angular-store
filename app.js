1032f2ff20af9ebe2caac15e171422688c9edb61
Miroslav Martynov <marty.mircha@gmail.com>
2014-09-19 14:46:10 +0300

    Tabs workaround

--- app.js
+++ app.js
@@ -9,20 +9,43 @@
     this.products = gem;
   });
 
+  app.controller('PanelController', function () {
+
+  });
+
   var gem = [
     {
       name       : 'Wow gem',
       price      : 2.65,
       description: "Some description",
       canPurchase: true,
-      soldOut    : false
+      soldOut    : false,
+      images: [
+        "images/hokkey.jpg",
+        "images/horror.jpg"
+      ]
     },
     {
       name       : 'Gadamn gem',
-      price      : 2.34,
+      price      : 2,
       description: "Some description",
       canPurchase: false,
-      soldOut    : false
+      soldOut    : true,
+      images: [
+        "images/humor.jpg",
+        "images/kamish.jpg"
+      ]
+    },
+    {
+      name       : 'Waaat?',
+      price      : 23.45,
+      description: "Some description",
+      canPurchase: false,
+      soldOut    : false,
+      images: [
+        "images/kashey.jpg",
+        "images/horror.jpg"
+      ]
     }
   ]
 })();
