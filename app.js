28266d20ca3b23295b992a6b48aec36288abfb43
Miroslav Martynov <marty.mircha@gmail.com>
2014-09-19 15:52:17 +0300

    Tabs controller

--- app.js
+++ app.js
@@ -10,7 +10,13 @@
   });
 
   app.controller('PanelController', function () {
-
+    this.tab = 1;
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
   });
 
   var gem = [
