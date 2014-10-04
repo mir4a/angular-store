2fe00d1a988064d3a35320d3aee9b5e0c4ded7b5
Miroslav Martynov <marty.mircha@gmail.com>
2014-10-04 16:44:11 +0300

    Forgotten gallery

--- app.js
+++ app.js
@@ -43,6 +43,20 @@
     }
   });
 
+  app.directive('productGallery', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/product-gallery.html',
+      controller: function() {
+        this.current = 0;
+        this.setCurrent = function(imageNumber){
+          this.current = imageNumber || 0;
+        };
+      },
+      controllerAs: 'gallery'
+    };
+  });
+
 
   var gem = [
     {
