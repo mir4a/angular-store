ae5e748d720ab20ba6329d8a28a5a766b3fd911e
Miroslav Martynov <marty.mircha@gmail.com>
2014-10-04 15:15:27 +0300

    Added date now, validation and styles for it

--- app.js
+++ app.js
@@ -19,6 +19,17 @@
     };
   });
 
+  app.controller("ReviewController", function() {
+    this.review = {};
+
+    this.addReview = function(product) {
+      this.review.createdOn = Date.now();
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+
+
   var gem = [
     {
       name       : 'Wow gem',
